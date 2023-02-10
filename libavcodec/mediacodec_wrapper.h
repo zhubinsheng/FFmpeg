1a2da6c753f16bc2a98ade9d5c2a1cbe4aec2c7d
zhubinsheng <zhubinsheng@yvr.cn>
2023-02-10 18:37:30 +0800

    123

--- libavcodec/mediacodec_wrapper.h
+++ libavcodec/mediacodec_wrapper.h
@@ -126,6 +126,8 @@ int ff_AMediaCodec_getConfigureFlagEncode(FFAMediaCodec *codec);
 
 int ff_AMediaCodec_cleanOutputBuffers(FFAMediaCodec *codec);
 
+int ff_AMediaCodec_signalEndOfInputStream(FFAMediaCodec *codec)
+
 int ff_Build_SDK_INT(AVCodecContext *avctx);
 
 #endif /* AVCODEC_MEDIACODEC_WRAPPER_H */
